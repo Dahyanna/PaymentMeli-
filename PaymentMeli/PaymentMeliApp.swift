a435bfe55ba4910a8a1a8527ca159c76891bfc5c
Dahyanna <dahyanna.hernandez@externos-cl.cencosud.com>
2023-04-04 13:56:00 -0400

    Initial Commit

---
+++ PaymentMeli/PaymentMeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentMeliApp.swift
+//  PaymentMeli
+//
+//  Created by Dahyanna Hernández on 04-04-23.
+//
+
+import SwiftUI
+
+@main
+struct PaymentMeliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
